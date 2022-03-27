b0acfd2b510d877d1a6441f19577345e3fb0cb8f
Alviss Raghnall <isiomastewart@gmail.com>
2022-03-27 04:04:42 +0100

    removed package lock json

--- client/static/js/projects.js
+++ client/static/js/projects.js
@@ -1,10 +1,4 @@
 
-const mt100mLangs = fetch("https://api.github.com/repos/alvissraghnall/mytop100movies/languages")
-.then(res => res.json())
-.catch(err => console.error(err));
-
-window.addEventListener("DOMContentLoaded", async function(ev) {
-    console.log(await mt100mLangs);
-    console.log("xy");
-})
-
+// const mt100mLangs = fetch("https://api.github.com/repos/alvissraghnall/mytop100movies/languages")
+// .then(res => res.json())
+// .catch(err => console.error(err));
